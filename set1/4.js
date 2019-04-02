3a539bc3f6de18035963851782f108695d29f985
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-02 00:19:48 -0300

    Refactor

--- set1/4.js
+++ set1/4.js
@@ -2,44 +2,20 @@ const fs    = require('fs')
 
 /* Source: http://www.oxfordmathcenter.com/drupal7/node/353 */
 const charFrequency = {
-  e: 0.12702,
-  t: 0.09056,
-  a: 0.08167,
-  o: 0.07507,
-  i: 0.06966,
-  n: 0.06749,
-  s: 0.06327,
-  h: 0.06094,
-  r: 0.05987,
-  d: 0.04253,
-  l: 0.04025,
-  c: 0.02782,
-  u: 0.02758,
-  m: 0.02406,
-  w: 0.02360,
-  f: 0.02228,
-  g: 0.02015,
-  y: 0.01974,
-  p: 0.01929,
-  b: 0.01492,
-  v: 0.00978,
-  k: 0.00772,
-  j: 0.00153,
-  x: 0.00150,
-  q: 0.00095,
-  z: 0.00074
+  e: 0.12702, t: 0.09056, a: 0.08167, o: 0.07507, i: 0.06966, n: 0.06749,
+  s: 0.06327, h: 0.06094, r: 0.05987, d: 0.04253, l: 0.04025, c: 0.02782,
+  u: 0.02758, m: 0.02406, w: 0.02360, f: 0.02228, g: 0.02015, y: 0.01974,
+  p: 0.01929, b: 0.01492, v: 0.00978, k: 0.00772, j: 0.00153, x: 0.00150,
+  q: 0.00095, z: 0.00074
 }
 
 function calculateFrequency (string) {
-  var sum = 0
-  string.toLowerCase().split('').forEach((letter) => {
-    sum += charFrequency[letter] || 0
-  })
-  return sum
+  return string.toLowerCase().split('')
+    .reduce((sum, value) => sum + (charFrequency[value] || 0), 0)
 }
 
-var file = fs.readFileSync('4.txt', 'utf-8')
-var lines = file.split('\n')
+var file           = fs.readFileSync('4.txt', 'utf-8')
+var lines          = file.split('\n')
 var frequenciesArr = []
 
 lines.forEach((line) => {
@@ -59,4 +35,3 @@ lines.forEach((line) => {
 
 frequenciesArr.sort((a, b) => b[1] - a[1])
 console.log(frequenciesArr.shift());
-
