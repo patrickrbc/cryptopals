7a93f4794fdb63c0338746c99d7c4e572eb59913
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-01 22:16:08 -0300

    Add fourth

--- set1/3.js
+++ set1/3.js
@@ -1,13 +1,15 @@
 
-const input = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
+const buf = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
 
-var result = Buffer.alloc(input.length)
-var letter = 65
+var result = Buffer.alloc(buf.length)
+var msg
 
-for (var i = 0, len = 23; i < len; i++) {
-  for (var j = 0, len = input.length; j < len; j++)
-    result[j] = input[j] ^ letter
+for (let letter = 0, len = 256; letter < len; letter++) {
+  for (let i = 0, len = buf.length; i < len; i++)
+    result[i] = buf[i] ^ letter
 
-  console.log(result.toString())
-  letter++
+  msg = result.toString()
+
+  if (!/[^\x00-\x7E]/g.test(msg) && /\s/.test(msg))
+    console.log(msg)
 }
