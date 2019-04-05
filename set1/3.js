37a9b871c517e39e9a903acf9552391882b92748
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-05 09:31:45 -0300

    Refactor third

--- set1/3.js
+++ set1/3.js
@@ -1,15 +1,10 @@
 
-const buf = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
+const assert = require('assert')
+const XOR    = require('../lib/xor')
+const buf    = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
 
-var result = Buffer.alloc(buf.length)
-var msg
+var result = XOR.bruteForceSingleByte(buf)
 
-for (let letter = 0, len = 256; letter < len; letter++) {
-  for (let i = 0, len = buf.length; i < len; i++)
-    result[i] = buf[i] ^ letter
+assert.equal(result.message, 'Cooking MC\'s like a pound of bacon')
 
-  msg = result.toString()
-
-  if (!/[^\x00-\x7E]/g.test(msg) && /\s/.test(msg))
-    console.log(msg)
-}
+console.log(result);
