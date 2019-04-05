72ffaf0a642eb110eb96f19fbbbdc0447d2ceff2
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-05 08:33:09 -0300

    Refactor first

--- set1/1.js
+++ set1/1.js
@@ -1,7 +1,10 @@
 
-console.log(
-  Buffer
+const assert = require('assert')
+
+var result = Buffer
   .from('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d','hex')
   .toString('base64')
-)
 
+assert.equal('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t', result)
+
+console.log(result);
