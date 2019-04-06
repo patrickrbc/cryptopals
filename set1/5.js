26e95a91bb06dad97bc63febea3541d273d3f6b6
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-06 19:02:24 -0300

    Sending key to XOR function as a buffer

--- set1/5.js
+++ set1/5.js
@@ -7,7 +7,9 @@ var msg = Buffer.from(
 `Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal` , 'ascii')
 
-var result = XOR.cipher(msg, KEY).toString('hex')
+var keyBuffer = Buffer.from(KEY, 'ascii')
+
+var result = XOR.cipher(msg, keyBuffer).toString('hex')
 
 assert.equal(result, '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
 
