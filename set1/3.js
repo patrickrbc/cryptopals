3c0c8e9fc72e95e792267306a4ec91622c780d01
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-06 19:52:17 -0300

    Fix function call

--- set1/3.js
+++ set1/3.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const XOR    = require('../lib/xor')
 const buf    = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
 
-var result = XOR.breakSingle(buf)
+var result = XOR.breakSingleByte(buf)
 
 assert.equal(result.message, 'Cooking MC\'s like a pound of bacon')
 
