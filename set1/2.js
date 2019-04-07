1bd2923c5dc298130c0ac38f6995b81f8c091329
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-07 12:50:24 -0300

    Refactor for the previous commit

--- set1/2.js
+++ set1/2.js
@@ -1,10 +1,10 @@
 
 const assert = require('assert')
 const XOR    = require('../lib/xor')
-const input  = '1c0111001f010100061a024b53535009181c'
-const key    = '686974207468652062756c6c277320657965'
+const input  = Buffer.from('1c0111001f010100061a024b53535009181c', 'hex')
+const key    = Buffer.from('686974207468652062756c6c277320657965', 'hex')
 
-var result = Buffer.from(XOR.fixed(input, key), 'ascii')
+var result = Buffer.from(XOR.cipher(input, key), 'ascii')
   .toString('hex')
 
 assert.equal(result, '746865206b696420646f6e277420706c6179')
