7585ad699db540392c00f6c04519f81ad58a06fa
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-07 13:14:25 -0300

    Fix second

--- set1/3.js
+++ set1/3.js
@@ -1,10 +1,9 @@
 
 const assert = require('assert')
 const XOR    = require('../lib/xor')
-const buf    = Buffer.from('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', 'hex')
 
-var result = XOR.breakSingleByte(buf)
+var result = XOR.breakSingleByte('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
 
-assert.equal(result.message, 'Cooking MC\'s like a pound of bacon')
+assert.equal(result.msg, 'Cooking MC\'s like a pound of bacon')
 
 console.log(result);
