371d8b76922526ac7e6769deb82da4a2b1e52249
Patrick Costa <patrickrbcosta@gmail.com>
2019-04-07 15:13:07 -0300

    Refactor sixth

--- set1/6.js
+++ set1/6.js
@@ -4,52 +4,25 @@ const assert = require('assert')
 const XOR    = require('../lib/xor')
 const Util   = require('../lib//util')
 
-var keySize, blocks, fileDecoded, result
+var key, keysize, blocks, msg, result
 var keyScores = []
 
-fileDecoded = Buffer.from(fs.readFileSync('../res/6.txt', 'utf-8'), 'base64')
-
-keySize     = XOR.findKeySize(fileDecoded)
-
-blocks      = transposeBlocks(divideInBlocks(fileDecoded, keySize), keySize)
-
-result      = XOR.cipher(fileDecoded, findKey(blocks)).toString()
+msg     = Buffer.from(fs.readFileSync('../res/6.txt', 'utf-8'), 'base64')
+keysize = XOR.findKeySize(msg)
+blocks  = Util.divideInBlocks(msg, keysize)
+blocks  = Util.transposeBlocks(blocks, keysize)
+key     = findKey(blocks)
+result  = XOR.cipher(msg, key).toString()
 
 assert.equal(result.length, 2876)
 
 console.log(result)
 
 function findKey (transposedBlocks) {
-  var keys = []
-  transposedBlocks.forEach((block) => {
-    var a = XOR.breakSingleByte(block)
-    keys.push(a.key)
+  var key = ''
+  transposedBlocks.forEach(block => {
+    key += String.fromCharCode(XOR.breakSingleByte(block).key)
   })
 
-  var KEY = ''
-  for (let i = 0, len = keys.length; i < len; i++)
-    KEY += String.fromCharCode(keys[i])
-
-  return Buffer.from(KEY, 'ascii')
-}
-
-function divideInBlocks (msg, KEYSIZE) {
-  var blocks = []
-  for (let i = 0, len = Math.floor(msg.length / KEYSIZE); i < len; i++)
-    blocks.push(Buffer.from(msg).slice(i * KEYSIZE, (i * KEYSIZE) + KEYSIZE))
-
-  return blocks
-}
-
-function transposeBlocks (blocks) {
-  var transposedBlocks = []
-  var transposedBlockBuffer = Buffer.alloc(blocks.length)
-  var blockSize = blocks[0].length
-  for (let i = 0, len = blockSize; i < len; i++) {
-    for (let j = 0, len = blocks.length; j < len; j++)
-      transposedBlockBuffer[j] = blocks[j][i]
-    transposedBlocks.push(Buffer.from(transposedBlockBuffer))
-  }
-
-  return transposedBlocks
+  return Buffer.from(key, 'ascii')
 }
